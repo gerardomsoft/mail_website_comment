8da3a0a52cd814e5d7487844c569d4e21ab08871
gerardomr8 <gerardo.mixteco@quadit.mx>
2022-11-11 19:19:39 -0600

    [ADD] welcome module

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name" : 'Modulo para recibir los comentarios del sitio web LAPI',
+    "version" : '15.0.0.1',
+    "author" : 'Mérida Soft',
+    "category" : 'Marketing',
+    "website" : "meridasoft.com",
+    "description" : """
+            Modulo para recibir los comentarios del sitio web.
+     """,
+    "init_xml" : [],
+    "depends" : ['base', 'portal'],
+    "data" : [
+    ],
+    "demo_xml":[],
+    "test":[],
+    "installable": True,
+    "images": [],
+    "auto_install": False,
+}
+
